9d59d4262d0ea7bc9c809054bda4f8beb51fe352
Anjaboii <135606500+Anjaboii@users.noreply.github.com>
2023-06-26 15:47:15 +0530

    Add files via upload

---
+++ practical02/Practical03-04.Area , diameter and circumference.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int main()
+{
+ {
+float radius,diameter,circumference,area,pi=3.14159;
+printf("Enter the radius ");
+scanf("%f",&radius);
+
+diameter=radius * 2;
+circumference= 2 * pi * radius;
+area = pi * radius * radius;
+
+printf("The diameter of the circle is %f\n",diameter);
+printf("The circumference of the circle is %f\n",circumference);
+printf("The area of the circle is %f",area);
+}
+
+
+}
+}
