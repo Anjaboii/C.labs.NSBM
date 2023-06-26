06be9cea7aa5f54ad64901397e2ce44cafd3814a
Anjaboii <135606500+Anjaboii@users.noreply.github.com>
2023-06-26 15:50:54 +0530

    Add files via upload

---
+++ practical02/Practical03-03)Integer equivalent.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int main()
+
+{
+printf("The integer equivalents");
+printf("A = %d\n",'A');
+printf("B = %d\n",'B');
+printf("C = %d\n",'C');
+printf("a = %d\n",'a');
+printf("b = %d\n",'b');
+printf("c = %d\n",'c');
+printf("0 = %d\n",'0');
+printf("1 = %d\n",'1');
+printf("2 = %d\n",'2');
+printf("$ = %d\n",'$');
+printf("* = %d\n",'*');
+printf("+ = %d\n",'+');
+printf("/ = %d\n",'/');
+printf("blank character = %d",' ');
+}
+
+
+
+}
