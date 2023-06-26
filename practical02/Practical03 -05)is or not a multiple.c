2bf335031782a4f3ea1465696ff3039e407b4ffd
Anjaboii <135606500+Anjaboii@users.noreply.github.com>
2023-06-26 15:49:42 +0530

    Add files via upload

---
+++ practical02/Practical03 -05)is or not a multiple.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int main()
+
+{
+ int no1,no2;
+
+printf("Enter your first integer ");
+scanf("%d",&no1);
+
+printf("Enter your second integer ");
+scanf("%d",&no2);
+
+if (no1%no2==0)
+printf("%d is a multiple",no1,no2);
+else
+printf("%d is not a multiple",no1,no2);
+}
+
+
+}
